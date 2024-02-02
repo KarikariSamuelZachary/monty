047a592d0313477328249ec3cd38262cb3ff1933
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-02-02 01:27:31 +0100

    Done

--- monty.h
+++ monty.h
@@ -3,21 +3,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include <unistd.h>
 
-typedef struct stack_s {
-    int n;
-    struct stack_s *prev;
-    struct stack_s *next;
-} stack_t;
-
-typedef struct instruction_s {
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
     char *opcode;
     void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void monty_execute(char *filename);
-void tokenize_and_execute(char *line, unsigned int line_number);
+extern global_t vglo;
 
-#endif /* MONTY_H */
+/* opcode_instructuions*/
+void push_element(stack_t **stack, unsigned int line_number);
+void print_stack(stack_t **stack, unsigned int line_number);
+void print_top(stack_t **doubly, unsigned int cline);
+void pop_element(stack_t **doubly, unsigned int cline);
+void swap_elements(stack_t **doubly, unsigned int cline);
+void stack(stack_t **doubly, unsigned int cline);
+void add_top_two(stack_t **doubly, unsigned int cline);
+void nop(stack_t **doubly, unsigned int cline);
+
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
+
+int _sch(char *s, char c);
+char *_strtoky(char *s, char *d);
+int _strcmp(char *s1, char *s2);
+
+void free_vglo(void);
+
+#endif
---
+++ opc_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - does nothing
+ *
+ * @doubly: head of the linked list
+ * @cline: line number
+ */
+void _nop(stack_t **doubly, unsigned int cline)
+{
+	(void)doubly;
+	(void)cline;
+}
